2866b4bbd006d2b04d5c78da4020d5f99cc67a15
deepvn98 <deepvn988@gmail.com>
2021-07-30 14:39:47 +0700

    add anotation for songcontroller

--- src/main/java/com/karaoke/mp3project/controller/SongController.java
+++ src/main/java/com/karaoke/mp3project/controller/SongController.java
@@ -1,5 +1,16 @@
 package com.karaoke.mp3project.controller;
 
+import com.karaoke.mp3project.service.impl.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("song")
+@RestController
 public class SongController {
+    @Autowired
+    private SongService songService;
 
 }
