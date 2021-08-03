188775c414614015b92a1ad0a423aba154372572
deepvn98 <deepvn988@gmail.com>
2021-08-03 18:00:43 +0700

    fix entity_song, singer, crud-singer

--- src/main/java/com/karaoke/mp3project/service/ISingerService.java
+++ src/main/java/com/karaoke/mp3project/service/ISingerService.java
@@ -10,10 +10,10 @@ import java.util.Optional;
 public interface ISingerService {
     ArrayList<Singer> findAll();
 
-    Iterable<Singer> findByName(String name);
+    Singer findByName(String name);
 
     Optional<Singer> findOne(Long id);
 
-    void saveSinger(Singer singer);
+    Singer saveSinger(Singer singer);
     void deleteSinger(Long id);
 }
