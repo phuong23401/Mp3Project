c720b9c3901647fe6a3b5038c0989d2ba45fd996
ha271097 <nguyenquangha2710@gmail.com>
2021-08-05 21:12:12 +0700

    fix page admin my song

--- src/main/java/com/karaoke/mp3project/controller/SongController.java
+++ src/main/java/com/karaoke/mp3project/controller/SongController.java
@@ -1,8 +1,10 @@
 package com.karaoke.mp3project.controller;
 
 import com.karaoke.mp3project.dto.respon.MessageResponse;
+import com.karaoke.mp3project.model.Role;
 import com.karaoke.mp3project.model.Song;
 import com.karaoke.mp3project.model.User;
+import com.karaoke.mp3project.security.userprincipal.UserDtService;
 import com.karaoke.mp3project.service.impl.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,13 +14,16 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @CrossOrigin(origins = "*")
 @RequestMapping("song")
@@ -26,6 +31,9 @@ import java.util.Optional;
 public class SongController {
     @Autowired
     private SongService songService;
+
+    @Autowired
+    private UserDtService userDtService;
 
     @PostMapping("/create")
     public ResponseEntity<?> createSong(@Valid @RequestBody Song song) {
@@ -58,11 +66,11 @@ public class SongController {
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateSong(@PathVariable Long id,
-                                        @Valid @RequestBody Song newSong){
+                                        @Valid @RequestBody Song newSong) {
         Optional<Song> song = songService.findOne(id);
         if (!song.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }else {
+        } else {
 //            if (newSong.getAvatarUrl() == null || newSong.getAvatarUrl().trim().isEmpty()) {
 //                return new ResponseEntity<>(new MessageResponse("noavatar"), HttpStatus.OK);
 //            }
@@ -78,24 +86,51 @@ public class SongController {
             return new ResponseEntity<>(new MessageResponse("Done"), HttpStatus.OK);
         }
     }
+
     @GetMapping("/page/song")
-    public ResponseEntity<?> pageSong(@PageableDefault(sort = "name", direction = Sort.Direction.ASC) Pageable pageable){
+    public ResponseEntity<?> pageSong(@PageableDefault(sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<Song> songPage = songService.findAllSong(pageable);
-        if(songPage.isEmpty()){
+        if (songPage.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(songPage, HttpStatus.OK);
     }
 
 
-
     @RequestMapping(value = "/songs", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Song>> getAllSong() {
+        User userCurrent = userDtService.getCurrentUser();
         List<Song> songList = songService.findAll();
         if (songList.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(songList, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/mysong", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Song>> getMySong() {
+        User userCurrent = userDtService.getCurrentUser();
+        List<Song> listSong = new ArrayList<>();
+        Set<Role> roles = userCurrent.getRole();
+        boolean check = false;
+        for (Role r: roles
+             ) {
+            if(r.getName().toString().equals("ROLE_ADMIN")){
+                check = true;
+            }
+        }
+        if (check) {
+            listSong = songService.findAll();
+            if (listSong.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        } else {
+            listSong = songService.findSongByUser(userCurrent.getId());
+            if (listSong.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        }
+        return new ResponseEntity<>(listSong, HttpStatus.OK);
     }
 
     @GetMapping(value = "/search-song", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -104,20 +139,23 @@ public class SongController {
         List<Song> songList = songService.findAllByNameSong(nameSong);
         return new ResponseEntity<>(songList, HttpStatus.OK);
     }
+
     @PostMapping("/search")
-    public ResponseEntity<Iterable<Song>> getAllSongByName(@RequestBody String name){
+    public ResponseEntity<Iterable<Song>> getAllSongByName(@RequestBody String name) {
         Iterable<Song> songs = songService.findByName(name);
         return new ResponseEntity<>(songs, HttpStatus.OK);
     }
+
     @GetMapping("/song/{id}")
-    public ResponseEntity<Song> getSongById(@PathVariable("id") Long id){
+    public ResponseEntity<Song> getSongById(@PathVariable("id") Long id) {
         Song song = songService.findOneName(id);
         return new ResponseEntity<>(song, HttpStatus.OK);
     }
+
     @GetMapping("/songs/{id}")
-    public ResponseEntity<?> getsongById(@PathVariable Long id){
+    public ResponseEntity<?> getsongById(@PathVariable Long id) {
         Optional<Song> song = songService.findOne(id);
-        if(!song.isPresent()){
+        if (!song.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(song, HttpStatus.OK);
@@ -128,37 +166,38 @@ public class SongController {
         List<Song> songList = songService.findAllByCreationTimeOrderByCreationTime();
         return new ResponseEntity<>(songList, HttpStatus.OK);
     }
-//    @GetMapping("/toplisten")
+
+    //    @GetMapping("/toplisten")
 //    public ResponseEntity<?> topListen() {
 //        List<Song> songs = songService.findAllByOrderByListenSong();
 //        return new ResponseEntity<>(songs,HttpStatus.OK);
 //    }
     @GetMapping("/count-listen-song/{id}")
-    public ResponseEntity<?> getSongListenById(@PathVariable("id") Long id){
+    public ResponseEntity<?> getSongListenById(@PathVariable("id") Long id) {
         try {
             Song song = songService.findOne(id).orElseThrow(EntityNotFoundException::new);
-            song.setNumberOfView(song.getNumberOfView()+1);
+            song.setNumberOfView(song.getNumberOfView() + 1);
             songService.saveSong(song);
-            return new ResponseEntity<>(song,HttpStatus.OK);
-        } catch (EntityNotFoundException e){
+            return new ResponseEntity<>(song, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
             return new ResponseEntity<>(new MessageResponse(e.getMessage()), HttpStatus.NOT_FOUND);
         }
     }
+
     @GetMapping("/search-param")
     //singer,String user,String author,String name
-    public  ResponseEntity<Iterable<Song>> searchParam(@RequestParam(name = "singer") String singer,
-                                                       @RequestParam(name = "user") User user,
-                                                       @RequestParam(name = "author") String author,
-                                                       @RequestParam(name = "name") String name){
+    public ResponseEntity<Iterable<Song>> searchParam(@RequestParam(name = "singer") String singer,
+                                                      @RequestParam(name = "user") User user,
+                                                      @RequestParam(name = "author") String author,
+                                                      @RequestParam(name = "name") String name) {
         Iterable<Song> songs = songService.findAllBySingerContainingAndUserContainingAndAuthorContainingAndNameContaining(singer, user, author, name);
-        if (songs == null){
+        if (songs == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(songs,HttpStatus.OK);
+        return new ResponseEntity<>(songs, HttpStatus.OK);
 
 
     }
-
 
 
 }
