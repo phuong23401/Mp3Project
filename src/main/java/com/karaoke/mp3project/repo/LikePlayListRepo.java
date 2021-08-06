2c443fe161d353e21b67349fb17d68ba8f5ddfd1
phuong23401 <hoaphuong23401@gmail.com>
2021-08-06 12:13:23 +0700

    update home controller

--- src/main/java/com/karaoke/mp3project/repo/LikePlayListRepo.java
+++ src/main/java/com/karaoke/mp3project/repo/LikePlayListRepo.java
@@ -1,11 +1,16 @@
 package com.karaoke.mp3project.repo;
 
 import com.karaoke.mp3project.model.LikePlayList;
+import com.karaoke.mp3project.model.LikeSong;
 import com.karaoke.mp3project.model.PlayList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LikePlayListRepo extends JpaRepository<LikePlayList, Long> {
     Iterable<LikePlayList> findAllByPlaylist(PlayList playlist);
+
+    List<LikePlayList> findByUserContaining(String username);
 }
