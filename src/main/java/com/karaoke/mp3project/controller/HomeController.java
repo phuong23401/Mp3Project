3f9d4dd2afe6cf77f68f9c6bbd54c79fc325332d
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-08-09 10:19:00 +0700

    fix home controller

--- src/main/java/com/karaoke/mp3project/controller/HomeController.java
+++ src/main/java/com/karaoke/mp3project/controller/HomeController.java
@@ -199,5 +199,14 @@ public class HomeController {
         PlayList playlist = playlistService.findOnePlayList(id);
         return new ResponseEntity<>(playlist, HttpStatus.OK);
     }
+    @GetMapping("/getallsong/{id}")
+    public ResponseEntity<Iterable<Song>> getAllSongInPlaylist(@PathVariable Long id) {
+        Iterable<Song> songList = playlistService.findAllSongInPlaylist(id);
+        return new ResponseEntity<>(songList, HttpStatus.OK);
+    }
+    @GetMapping("/get/{id}")
+    private ResponseEntity<Optional<PlayList>> getPlaylist(@PathVariable Long id){
+        return new ResponseEntity<>(playlistService.findById(id), HttpStatus.OK) ;
+    }
 
 }
